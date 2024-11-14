3a4800c288f43509309972575e7c347f54af9326
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-14 16:18:23 -0300

    starting dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django import forms
 
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("fala fiote")
+    return render(request, 'dashboard/home.html')
